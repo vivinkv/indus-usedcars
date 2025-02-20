138b73318068b70d2e4338b1b937d8bceeea99bb
vivinkv <vivin@spiderworks.in>
2025-02-20 16:33:03 +0530

    update

--- src/api/menu/controllers/menus.js
+++ src/api/menu/controllers/menus.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * A set of functions called "actions" for `menus`
@@ -7,31 +7,80 @@
 module.exports = {
   menus: async (ctx, next) => {
     try {
-      const general=await strapi.documents('api::general.general').findFirst({
-        populate:'*'
-      })
-      const location=await strapi.documents('api::location.location').findMany({});
+      const general = await strapi.documents("api::general.general").findFirst({
+        populate: "*",
+      });
+      const location = await strapi
+        .documents("api::location.location")
+        .findMany({});
       console.log(location);
-      
-      const menu=await strapi.documents('api::menu.menu').findFirst({
-        populate:{
-          Header:{
-            populate:'*'
+
+      const menu = await strapi.documents("api::menu.menu").findFirst({
+        populate: {
+          Header: {
+            populate: "*",
           },
-          Footer:{ 
-            populate:{
-              Page:{
-                populate:'*'
-              }
-            }
-          }
-        }
-      })
-      ctx.status=200
-      ctx.body ={data:{...menu,...general,location:location}};
+          Footer: {
+            populate: {
+              Page: {
+                populate: "*",
+              },
+            },
+          },
+        },
+      });
+      ctx.status = 200;
+      ctx.body = { data: { ...menu, ...general, location: location } };
     } catch (err) {
-      ctx.status=500
+      ctx.status = 500;
       ctx.body = err;
     }
-  }
+  },
+  searchCar: async (ctx, next) => {
+    try {
+      console.log('inside',ctx.query);
+      
+      const { query } = ctx.query;
+      let carsList;
+      if (query) {
+        carsList = await strapi.documents("api::car.car").findMany({
+          filters: {
+            Name: {
+              $containsi: query,
+            },
+          },
+          populate: {
+            Outlet: {
+              populate: "*",
+            },
+          },
+          limit: 6,
+        });
+      } else {
+        console.log('yes');
+        
+        carsList = await strapi.documents("api::car.car").findMany({
+          populate: {
+            Outlet: {
+              populate: "*",
+            },
+          },
+          limit:6 
+        });
+      }
+      console.log('finish');
+      
+      ctx.status = 200;
+      ctx.body = {
+        data: carsList,
+      };
+    } catch (error) { 
+      console.log(error);
+      
+      ctx.status = 500;
+      ctx.body = {
+        error: error?.message,
+      };
+    }
+  },
 };
--- src/api/menu/routes/menus.js
+++ src/api/menu/routes/menus.js
@@ -1,14 +1,27 @@
-'use strict'
+"use strict";
 
-module.exports={
-    routes:[{
-        method:'GET',
-        path:'/menu/main',
-        handler:'menus.menus',
-        config:{
-            auth:false,
-            policies:[],
-            middleware:[]
-        }
-    }]
-}
+module.exports = {
+  routes: [
+    {
+        method: "GET",
+        path: "/menu/search",
+        handler: "menus.searchCar",
+        config: {
+          auth: false,
+          policies: [], 
+          middleware: [],
+        },
+      },
+    {
+      method: "GET",
+      path: "/menu/main",
+      handler: "menus.menus",
+      config: {
+        auth: false,
+        policies: [],
+        middleware: [],
+      },
+    },
+    
+  ],
+};
