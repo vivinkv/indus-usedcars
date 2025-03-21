12970b4efe493f2b2a6285dca07b4d96129b90b2
vivinkv6 <vivinkv0@gmail.com>
2025-03-21 18:07:47 +0530

    add email configuration

---
+++ config/email.js
@@ -0,0 +1,15 @@
+module.exports = {
+  provider: '@strapi/provider-email-nodemailer',
+  providerOptions: {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  },
+  settings: {
+    defaultFrom: process.env.SMTP_USERNAME,
+    defaultReplyTo: process.env.SMTP_USERNAME,
+  },
+};
